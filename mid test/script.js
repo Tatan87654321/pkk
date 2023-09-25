8abd6b33f20c18464d1d930d77abc5b342eede4d
Tatan87654321 <145957388+Tatan87654321@users.noreply.github.com>
2023-09-25 14:04:44 +0700

    Add files via upload

---
+++ mid test/script.js
@@ -0,0 +1,6 @@
+document,addEventListener("DOMContentLoaded" , function() {
+    const tasklist = document.getElementById ("task-list");
+    const newtaskinpu = document.getElementById("new-task");
+    const addTaskButton = document.getElementById("add-task");
+
+addTaskButton.addEventListener("click" , function() {}
